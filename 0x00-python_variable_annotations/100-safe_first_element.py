87a1beb78e4916d70b6ead98c5ab5ea3af0543bf
2022phyro <phyrokelstein2@gmail.com>
2023-07-06 14:52:23 +0100

    debugging no 10

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,6 +4,7 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Checks if the element is a sequence"""
     if lst:
         return lst[0]
     else:
