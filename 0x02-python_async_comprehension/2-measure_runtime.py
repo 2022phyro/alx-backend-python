20d8cc1fc75e8d02870a7c85de42108cfa2fb0ce
2022phyro <phyrokelstein2@gmail.com>
2023-07-11 12:54:17 +0100

    Added documentation to the no 2 function

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,6 +9,9 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """Measure the runtime of running four co routines of
+    roughly ten seconds each. it should compute to near 10
+    seconds showing ud the advantages of parallelism"""
     routines = [async_comprehension() for _ in range(4)]
     tpc = perf_counter()
     await gather(*routines)
