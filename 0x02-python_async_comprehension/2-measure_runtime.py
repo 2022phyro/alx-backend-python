e1b521f4062c0ef43962ce7bdf790c221168edd3
2022phyro <phyrokelstein2@gmail.com>
2023-07-11 12:50:50 +0100

    Attempted no 2 and cleanup

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -3,10 +3,14 @@
 in parallel"""
 
 from asyncio import gather
-from time import time
+from time import perf_counter
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
-
+async def measure_runtime() -> float:
+    routines = [async_comprehension() for _ in range(4)]
+    tpc = perf_counter()
+    await gather(*routines)
+    timeup = perf_counter() - tpc
+    return timeup
--- 0x02-python_async_comprehension/main.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-async_comprehension = __import__('1-async_comprehension').async_comprehension
-
-
-async def main():
-    print(await async_comprehension())
-
-asyncio.run(main())
